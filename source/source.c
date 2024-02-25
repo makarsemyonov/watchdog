de57627ea250699417690c891e17a548e0903795
Makar Semyonov <124182670+GhostOfVitebsk@users.noreply.github.com>
2024-02-25 22:08:58 +0300

    watchdog-v0.2
    Added the ability to control the sniffing flow through the control flow, added the ability to track statistics of the current session

--- source/source.c
+++ source/source.c
@@ -16,7 +16,7 @@ void printDevices() {
   printf("Available interfaces:\n\n");
   for (pcap_if_t *d = alldevs; d != NULL; d = d->next) {
     if (d->flags & PCAP_IF_UP && d->flags & PCAP_IF_RUNNING) {
-      printf("-> %s\n", d->name);
+      printf("[-] %s\n", d->name);
     }
   }
   pcap_freealldevs(alldevs);
@@ -35,6 +35,10 @@ void handlePacket(const char *packet, struct pcap_pkthdr header, FILE *file) {
     strcpy(dst_ip, inet_ntoa(ip_header->ip_dst));
     fprintf(file, "[IPv4] Source IP: %s, Destination IP: %s, %d bytes\n",
             src_ip, dst_ip, header.len);
+    allcounter++;
+    ipv4_counter++;
+    ipv4_bytes += header.len;
+    all_bytes += header.len;
   } else if (ntohs(eth_header->ether_type) == ETHERTYPE_IPV6) {
     ip6_header = (struct ip6_hdr *)(packet + sizeof(struct ether_header));
     char src_ip[INET6_ADDRSTRLEN];
@@ -43,6 +47,10 @@ void handlePacket(const char *packet, struct pcap_pkthdr header, FILE *file) {
     inet_ntop(AF_INET6, &(ip6_header->ip6_dst), dst_ip, INET6_ADDRSTRLEN);
     fprintf(file, "[IPv6] Source IP: %s, Destination IP: %s, %d bytes\n",
             src_ip, dst_ip, header.len);
+    allcounter++;
+    ipv6_counter++;
+    ipv6_bytes += header.len;
+    all_bytes += header.len;
   }
 }
 
@@ -63,6 +71,12 @@ void sniff(const char *dev) {
     return;
   }
   while (exitFlag) {
+    pthread_mutex_lock(&mutex);
+    if (flag == 0) {
+      pthread_mutex_unlock(&mutex);
+      continue;
+    }
+    pthread_mutex_unlock(&mutex);
     packet = pcap_next(handle, &header);
     handlePacket(packet, header, file);
   }
@@ -74,9 +88,37 @@ void *commandThread(void *arg) {
   char command[100];
   while (1) {
     fgets(command, sizeof(command), stdin);
-    if (strcmp(command, "stop\n") == 0) {
+    if (strcmp(command, "/stop\n") == 0) {
       exitFlag = 0;
       break;
+    }
+    else if (strcmp(command, "/help\n") == 0) {
+      printf("/help - show help\n"
+             "/stop - stop the process\n"
+             "/pause - pause the process\n"
+             "/resume - resume the process\n"
+             "/stats - show current statistics\n");
+    }
+    else if (strcmp(command, "/pause\n") == 0) {
+      pthread_mutex_lock(&mutex);
+      flag = 0;
+      pthread_mutex_unlock(&mutex);
+      printf("Sniffing paused...\n");
+    }
+    else if (strcmp(command, "/resume\n") == 0) {
+      pthread_mutex_lock(&mutex);
+      flag = 1;
+      pthread_mutex_unlock(&mutex);
+      printf("Sniffing resumed\n");
+    }
+    else if (strcmp(command, "/stats\n") == 0) {
+      printf("    |\tpackets\t|\tbytes\n"
+             "IPv4| \t%d\t| \t%d\t\n"
+             "IPv6| \t%d\t| \t%d\t\n"
+             "All | \t%d\t| \t%d\t\n", ipv4_counter, ipv4_bytes, ipv6_counter, ipv6_bytes, allcounter, all_bytes);
+    }
+    else {
+      printf("Wrong command!\n");
     }
   }
   return NULL;
