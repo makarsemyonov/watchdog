9557539c49444688465e9daaaa98cdaf534df5c5
Makar Semyonov <124182670+GhostOfVitebsk@users.noreply.github.com>
2024-02-25 22:11:11 +0300

    watchdog-v0.1
    Added the ability to control the sniffing flow through the control flow, added the ability to track statistics of the current session

--- watchdog.c
+++ watchdog.c
@@ -8,19 +8,26 @@ int main() {
     return 0;
   }
 
+  printf("Commands: \n/help - show help\n"
+             "/stop - stop the process\n"
+             "/pause - pause the process\n"
+             "/resume - resume the process\n"
+             "/stats - show current session statistics\n\n");
+
   printDevices();
 
-  printf("\nEnter interface to sniff: \n");
+  printf("\nEnter interface to sniff: \n[+] ");
   fgets(dev, sizeof(dev), stdin);
   dev[strcspn(dev, "\n")] = '\0';
 
-  printf("\nSniffing on %s...\nEnter \"stop\" to stop\n", dev);
+  printf("\nSniffing on %s...\n\n", dev);
 
   pthread_t thread;
   pthread_create(&thread, NULL, commandThread, NULL);
-
+  pthread_mutex_init(&mutex, NULL);
   sniff(dev);
   printf("\nLogs were saved to sniff.log. Finishing... \n");
   pthread_join(thread, NULL);
+  pthread_mutex_destroy(&mutex);
   return 0;
 }
